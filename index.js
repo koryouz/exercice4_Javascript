6d14062f00b34cae54611874e3dc36fa35306179
DELHAY <delhayantoine@gmail.com>
2019-01-21 10:57:03 +0100

    Ajout de l'exercice 4

---
+++ index.js
@@ -0,0 +1,7 @@
+function toggle() {
+  var space = "\n"
+  var nom = document.getElementById("lastname").value;
+  var prenom = document.getElementById("firstname").value;
+  var ville = document.getElementById("city").value;
+  alert(nom+space+prenom+space+ville);
+}
