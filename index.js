a1ade931ab3e1bc29e6194e1423c58d4be419654
DELHAY <delhayantoine@gmail.com>
2019-01-24 11:26:34 +0100

    Ajout du Regex

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 function toggle() {
-  var space = "\n"
-  var nom = document.getElementById("lastname").value;
-  var prenom = document.getElementById("firstname").value;
-  var ville = document.getElementById("city").value;
-  alert(nom+space+prenom+space+ville);
+  var regexName = /^[a-zA-Z ]+$/;
+  var lastname = document.getElementById("lastname").value;
+  var firstname = document.getElementById("firstname").value;
+  var city = document.getElementById("city").value;
+  if (lastname.match(regexName) && firstname.match(regexName) && city.match(regexName)){
+    alert(lastname +"\n"+ firstname +"\n"+ city);
+  }else {
+    alert('Characteres invalides');
+  }
 }
